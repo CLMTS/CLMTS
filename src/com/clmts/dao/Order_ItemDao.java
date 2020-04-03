3f3e900b4e4d613fe09147b01ad04626d58016a9
SuhaoPro <su_cheer@163.com>
2020-04-03 21:01:53 +0800

    orderitemdao

--- src/com/clmts/dao/Order_ItemDao.java
+++ src/com/clmts/dao/Order_ItemDao.java
@@ -5,5 +5,12 @@ package com.clmts.dao;
  * @create_date 2020-04-02 17:47
  */
 public interface Order_ItemDao {
+    //添加订单
+    boolean addIteam(int item_id,int product_id,double price,String order_id) throws Exception;
+
+    //删除订单
+    boolean delIteam(int item_id,int product_id,double price,String order_id) throws Exception;
+
+
 
 }
