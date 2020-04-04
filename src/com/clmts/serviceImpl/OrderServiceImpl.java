14fee791ca3fea6258e8c5bb42ad317126084878
biaisfat <liguirun0927@163.com>
2020-04-04 16:57:43 +0800

    完成记录订单业务流程

--- src/com/clmts/serviceImpl/OrderServiceImpl.java
+++ src/com/clmts/serviceImpl/OrderServiceImpl.java
@@ -3,33 +3,68 @@ package com.clmts.serviceImpl;
 import com.clmts.bean.Manager;
 import com.clmts.dao.ManagerDao;
 import com.clmts.dao.OrderDao;
+import com.clmts.dao.Order_ItemDao;
 import com.clmts.daoImpl.ManagerDaoImpl;
 import com.clmts.daoImpl.OrderDaoImpl;
+import com.clmts.daoImpl.Order_ItemDaoImpl;
 import com.clmts.service.OrderService;
 import com.clmts.util.DBUtil;
 
 import java.sql.Connection;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class OrderServiceImpl implements OrderService {
     private Connection conn = null;
     @Override
-    public boolean takeOrder(String name, String phone, String address, int[] product_id, double[] price) {
+    public boolean takeOrder(int manager_id, String name, String phone, String address, int[] product_id, double[] price) {
         String order_id;
         String time;
-        String total;
+        double total = 0;
         OrderDao orderDao=new OrderDaoImpl();
-        ManagerDao managerDao=new ManagerDaoImpl();
-        conn= DBUtil.getConn();
+        Order_ItemDao itemDao = new Order_ItemDaoImpl();
+        /*
+        记录在order表的：
+            订单编号(timestamp+id)、时间、经理id、客户姓名、电话、地址
+        记录在order_item表的：
+            单独售价、商品编号、订单编号
+         */
+        DBUtil.getConn();
         boolean flag=false;
-        try{
-
-
+        try {
+            Date date = new Date();
+            // 1. 生成订单号
+            order_id = String.valueOf(date.getTime()) + manager_id;
+            // 2. 获取当前时间
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            time = sdf.format(date);
+            // 3. 计算总价
+            for (double p : price) {
+                total += p;
+            }
+            // 4. 插入order表
+            flag = orderDao.addOrder(order_id, time, manager_id, name, phone, address, total);
+            if (flag) {
+                System.out.println("failed to insert into table order");
+                System.out.println("-----------");
+                return false;
+            }
+            // 5. 插入order_item表
+            for (int i = 0; i < price.length; i++) {
+                flag = itemDao.addIteam(product_id[i], price[i], order_id);
+                if (flag) {
+                    System.out.println("failed to insert into table order_item");
+                    System.out.println("-----------");
+                    return false;
+                }
+            }
+            // 业务结束，关闭数据库连接
+            DBUtil.close();
         }catch (Exception e){
-            e.printStackTrace();}
-
-
-
-        return false;
+            System.out.println("OrderServiceImpl.takeOrder: Exception occurred");
+            e.printStackTrace();
+            System.out.println("-----------");
+        }
+        return flag;
     }
 }
